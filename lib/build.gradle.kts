c5c27d012a4171654c9e53d91f4eb890c9bb43b5
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-05-16 12:53:47 +0200

    Update build.gradle

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,24 +1,62 @@
-// Works with Gradle 6.3 and Java 14
-
-// To run this do:
-// ./gradlew run
-
-// To build a distribution zip:
-// ./gradlew distZip
-
 plugins {
-    id 'java'
-    id 'application'
-    id 'org.openjfx.javafxplugin' version '0.0.8'
-}
+    // Apply the java plugin to add support for Java
+    java
 
-mainClassName = "razorDB.App"
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
 
-javafx {
-    version = "14"
-    modules = [ 'javafx.controls', 'javafx.fxml']
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
     mavenCentral()
+}
+
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+
+val supportedPlatforms = listOf("linux", "mac", "win")
+val jUnitVersion = "5.7.1"
+val javaFxVersion = 15
+
+dependencies {
+    // Example library: Guava. Add what you need (and remove Guava if you don't use it)
+    // implementation("com.google.guava:guava:28.1-jre")
+
+    // JavaFX: comment out if you do not need them
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
+
+    // JUnit API and testing engine
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+    implementation("mysql:mysql-connector-java:8.0.29")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("razorDB.App")
 }
